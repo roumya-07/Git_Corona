e5ad17456c73965ff65ffc5c2e33875300f9856d
shyamsundar.baral <shyamsundar.baral@CSMBHUL1027>
2022-12-12 21:29:45 +0530

    Service and repository

---
+++ Git_Corona/Ṛepository/BaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git_Corona.Ṛepository
+{
+    public class BaseRepository
+    {
+        private readonly IConfiguration _configuration;
+        protected BaseRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
