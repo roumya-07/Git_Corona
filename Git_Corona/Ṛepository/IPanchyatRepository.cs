d42268f4f5dc4f75af9832e2a500013652b54296
sushanta.senapati <sushanta.senapati@CSMBHUL954>
2022-12-12 22:31:24 +0530

    Panchayat part add in repositary

---
+++ Git_Corona/Ṛepository/IPanchyatRepository.cs
@@ -0,0 +1,28 @@
+﻿using Git_Corona.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git_Corona.Ṛepository
+{
+   public interface IPanchyatRepository
+    {
+
+
+         Task<List<Panchyat>> GetAll();
+
+
+         Task<Panchyat> GetByID(string UserID);
+
+
+         Task<List<Panchyat>> GetByID(int ID);
+
+
+         Task<Panchyat> GetByUserID(string ID, string Pwd);
+
+         Task<int> UpdateCreateData(Panchyat obj);
+      
+
+    }
+}
---
+++ Git_Corona/Ṛepository/PanchyatRepository.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using Git_Corona.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git_Corona.Ṛepository
+{
+    public class PanchyatRepository : BaseRepository, IPanchyatRepository
+    {
+        string spName = "Sp_CoronaDetails";
+        public PanchyatRepository(IConfiguration conf) : base(conf)
+        {
+
+        }
+
+        public Task<List<Panchyat>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Panchyat> GetByID(string UserID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Panchyat>> GetByID(int ID)
+        {
+            var con = CreateConnection();
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@action", "Pselect");
+            param.Add("@BlockID", ID);
+
+            var x = await con.QueryAsync<Panchyat>(spName, param, commandType: CommandType.StoredProcedure);
+            return x.ToList();
+        }
+
+        public Task<Panchyat> GetByUserID(string ID, string Pwd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateCreateData(Panchyat obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
