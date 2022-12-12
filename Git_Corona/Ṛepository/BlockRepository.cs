51ccc0ab7fa99fbe1b3cae8896231ce8fd2c4fc8
sushanta.senapati <sushanta.senapati@CSMBHUL954>
2022-12-12 22:18:38 +0530

    ecde

---
+++ Git_Corona/Ṛepository/BlockRepository.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using Git_Corona.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git_Corona.Ṛepository
+{
+    public class BlockRepository:BaseRepository
+    {
+        string spName = "Sp_CoronaDetails";
+    public BlockRepository(IConfiguration conf) : base(conf)
+    {
+
+    }
+
+    public Task<List<Block>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Block> GetByID(string UserID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Block>> GetByID(int ID)
+    {
+        var con = CreateConnection();
+        DynamicParameters param = new DynamicParameters();
+        param.Add("@action", "Bselect");
+        param.Add("@stateID", ID);
+
+        var x = await con.QueryAsync<Block>(spName, param, commandType: CommandType.StoredProcedure);
+        return x.ToList();
+    }
+
+    public Task<Block> GetByUserID(string ID, string Pwd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> UpdateCreateData(Block obj)
+    {
+        throw new NotImplementedException();
+    }
+}
+}
---
+++ Git_Corona/Ṛepository/IBlockRepository.cs
@@ -0,0 +1,24 @@
+﻿using Git_Corona.Models;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git_Corona.Ṛepository
+{
+   public interface IBlockRepository
+    {
+
+         Task<List<Block>> GetAll();
+
+         Task<Block> GetByID(string UserID);
+         Task<List<Block>> GetByID(int ID);
+
+
+         Task<Block> GetByUserID(string ID, string Pwd);
+
+         Task<int> UpdateCreateData(Block obj);
+      
+    }
+}
