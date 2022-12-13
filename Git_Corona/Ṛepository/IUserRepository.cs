4a81324c6187a7550f2bffc492c6a97eb854162e
shyamsundar.baral <shyamsundar.baral@CSMBHUL1027>
2022-12-13 07:52:59 +0530

    UserRepository

---
+++ Git_Corona/Ṛepository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Git_Corona.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git_Corona.Ṛepository
+{
+    public interface IUserRepository
+    {
+        public Task<int> SaveUpdate(UserEntity ue);
+        public Task<UserEntity> GetByID(string UserID);
+        public Task<UserEntity> GetByUserID(string UserId, string Pswd);
+    }
+}
---
+++ Git_Corona/Ṛepository/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using Git_Corona.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git_Corona.Ṛepository
+{
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+        public UserRepository(IConfiguration configuration) : base(configuration)
+        {
+        }
+        public async Task<int> SaveUpdate(UserEntity ue)
+        {
+            var cn = CreateConnection();
+            if (cn.State == ConnectionState.Closed) cn.Open();
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@ID", ue.ID);
+            param.Add("@UserID", ue.UserID);
+            param.Add("@UserName", ue.UserName);
+            param.Add("@Password", ue.Password);
+            param.Add("@PhoneNo", ue.PhoneNo);
+            param.Add("@Email", ue.Email);
+            param.Add("@action", "Uinsert");
+            int x = cn.Execute("Sp_CoronaDetails", param, commandType: CommandType.StoredProcedure);
+            cn.Close();
+            return x;
+        }
+        public async Task<UserEntity> GetByID(string UserId)
+        {
+            var cn = CreateConnection();
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@UserID", UserId);
+            param.Add("@action", "USelect");
+            var lstprod = cn.Query<UserEntity>("Sp_CoronaDetails", param, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            cn.Close();
+            return lstprod;
+        }
+        public async Task<UserEntity> GetByUserID(string UserId, string Pswd)
+        {
+            var cn = CreateConnection();
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@UserID", UserId);
+            param.Add("@Password", Pswd);
+            param.Add("@action", "USelect");
+            var lstprod = cn.Query<UserEntity>("Sp_CoronaDetails", param, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            cn.Close();
+            return lstprod;
+        }
+    }
+}
