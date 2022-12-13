9f9733d98d38a39260194465d45ccd5163ad7621
shyamsundar.baral <shyamsundar.baral@CSMBHUL1027>
2022-12-13 09:58:15 +0530

    CoronaRepository

---
+++ Git_Corona/Ṛepository/CoronaRepository.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using Git_Corona.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git_Corona.Ṛepository
+{
+    public class CoronaRepository : BaseRepository, ICoronaRepository
+    {
+        string spName = "Sp_CoronaDetails";
+        public CoronaRepository(IConfiguration conf) : base(conf)
+        {
+
+        }
+
+        public async Task<List<CoronaStatus>> GetAll()
+        {
+            var con = CreateConnection();
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@action", "Ccount");
+            var _corona = await con.QueryAsync<CoronaStatus>(spName, param, commandType: CommandType.StoredProcedure);
+            return _corona.ToList();
+        }
+
+        public async Task<int> UpdateCreateData(CoronaStatus obj)
+        {
+            var con = CreateConnection();
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@action", "Cinsert");
+            param.Add("@PanchyatID", obj.PanchyatID);
+            param.Add("@CitizenName", obj.CitizenName);
+            param.Add("@AffectedDate", obj.AffectedDate);
+            param.Add("@status", obj.Status);
+            var x = await con.ExecuteAsync(spName, param, commandType: CommandType.StoredProcedure);
+            return x;
+        }
+    }
+}
---
+++ Git_Corona/Ṛepository/ICoronaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Git_Corona.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git_Corona.Ṛepository
+{
+    public interface ICoronaRepository
+    {
+        Task<List<CoronaStatus>> GetAll();
+        Task<int> UpdateCreateData(CoronaStatus obj);
+    }
+}
