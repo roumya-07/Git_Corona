c23ed102e0622d4dd8bbe09c2d167a0fabbc2780
shyamsundar.baral <shyamsundar.baral@CSMBHUL1027>
2022-12-13 10:02:53 +0530

    StateRepository

---
+++ Git_Corona/Ṛepository/IStateRepository.cs
@@ -0,0 +1,13 @@
+﻿using Git_Corona.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git_Corona.Ṛepository
+{
+    public interface IStateRepository
+    {
+        Task<List<State>> GetAll();
+    }
+}
---
+++ Git_Corona/Ṛepository/StateRepository.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Git_Corona.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git_Corona.Ṛepository
+{
+    public class StateRepository : BaseRepository, IStateRepository
+    {
+        string spName = "Sp_CoronaDetails";
+        public StateRepository(IConfiguration conf) : base(conf)
+        {
+
+        }
+
+        public async Task<List<State>> GetAll()
+        {
+            var con = CreateConnection();
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@action", "Sselect");
+            var _user = await con.QueryAsync<State>(spName, param, commandType: CommandType.StoredProcedure);
+            return _user.ToList();
+        }
+    }
+}
